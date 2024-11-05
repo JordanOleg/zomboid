c3c5565e1c4b54efb32379141eeebb73fb895c44
Oleg <jordanoleg2005@gmail.com>
2024-11-06 00:01:37 +0200

    fixed parse DataModel  - id, amount -> int(...)  - XMLReader, CSVReader, JSONReader fixed print in console  - PrinterModel add test  - test_readers

--- zomboid/main.py
+++ zomboid/main.py
@@ -1,12 +1,14 @@
 from pathlib import Path
 from src.io import Zomboid
 from src.io.csv_reader import CSVReader
+from src.io.json_reader import JSONReader
 from src.io.separator_model import SeparatorModel
 
-if __name__ == "__name__":
-    paths = Path("zomboid/.data/model.csv")
-    reader = CSVReader(paths)
+if __name__ == "__main__":
+    paths = Path("C:/Users/jorda/Desktop/Zomboid/zomboid/.data/json_model.json")
+    reader = JSONReader(paths)
     data_model = reader.read()
+    print(data_model)
     separator = SeparatorModel(source_models=data_model, skip=2, take=4)
     models = separator.get_models()
     reader = Zomboid(models) # Zomboid(models, reader.get_header())
