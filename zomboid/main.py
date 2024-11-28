a3659f5bef30c908169e3a394d055c046ab38268
JordanOleg <jordanoleg2005@gmail.com>
2024-11-28 15:46:06 +0200

    add requirement project

--- zomboid/main.py
+++ zomboid/main.py
@@ -4,11 +4,11 @@ from src.context import Context
 from src.paginator import Paginator
 
 if __name__ == "__main__":
-    paths = Path("zomboid/.data/model.json")
+    paths = Path("zomboid/.data/model.csv")
     context = Context(paths)
     reader = context.get_reader()
     data_model = reader.read()
-    separator = Paginator(source_models=data_model, skip=0, take=4)
+    separator = Paginator(source_models=data_model, skip=0, take=len(data_model))
     models = separator.get_models()
     reader = Zomboid(models)
     reader.print_models()
